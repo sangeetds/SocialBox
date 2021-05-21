65d15fc195cd19d44b75186e59ede163575b394d
sangeetds <sangeetnarayands@gmail.com>
2021-05-21 23:30:22 +0530

    Initial commit.

---
+++ app/src/main/java/com/socialbox/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.socialbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
