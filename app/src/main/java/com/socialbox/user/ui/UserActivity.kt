843d02c31ec22bbdaa3880eba334491a6a211dcf
sangeetds <sangeetnarayands@gmail.com>
2021-06-09 17:19:08 +0530

    * Added user activity * Password visibility icon

---
+++ app/src/main/java/com/socialbox/user/ui/UserActivity.kt
@@ -0,0 +1,12 @@
+package com.socialbox.user.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.socialbox.R
+
+class UserActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_user)
+  }
+}
