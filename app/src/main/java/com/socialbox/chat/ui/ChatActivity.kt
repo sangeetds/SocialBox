6d69e30d065a49e6293ac0ba52ab2b1254249c7f
sangeetds <sangeetnarayands@gmail.com>
2021-07-06 15:29:15 +0530

    Added chat screen

---
+++ app/src/main/java/com/socialbox/chat/ui/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.socialbox.chat.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.socialbox.R
+
+class ChatActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.chat_activity_layout)
+  }
+}
